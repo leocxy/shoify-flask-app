20af972b427ee3988f31e73043abfc59110ee123
Leo Chen <leo@pocketsquare.co.nz>
2022-05-09 09:27:26 +1200

    Add missing config

--- admin/src/admin/store/state.js
+++ admin/src/admin/store/state.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 export default Vue.observable({
     bridge: {}, // App Bridge
     rest_api: [
+        {name: 'check', url: '/admin/check'},
         {name: 'test_jwt', url: '/admin/test_jwt'},
-        {name: 'themes', url: '/admin/themes'},
     ]
 })
