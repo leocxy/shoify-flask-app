e6af115a271384391c5db291b6de141529f97552
Leo Chen <leo@pocketsquare.co.nz>
2022-05-09 09:21:57 +1200

    Added Check Updated function on Admin UI

--- admin/src/admin/App.vue
+++ admin/src/admin/App.vue
@@ -1,21 +1,38 @@
 <template>
     <PPage title="Pocket Square App">
         <router-view></router-view>
+        <PFooterHelp v-if="change"><PLink @click="reinstall()">Update</PLink> App permissions to get full functionality.</PFooterHelp>
     </PPage>
 </template>
 
 <script>
 import {mutation} from "@/store";
+import {errorCB} from "@/store/actions";
+import {getApi} from "@/store/getters";
 
 export default {
     name: 'App',
-    data: () => ({}),
+    data: () => ({
+        change: false
+    }),
+    methods: {
+        initial: function () {
+            // Init App Bridge
+            const apiKey = window?.AppInfo.apiKey,
+                host = window?.AppInfo.host;
+            this.$set(this.$http.defaults.headers.common, 'Authorization', `Bearer ${window?.AppInfo.jwtToken}`)
+            if (apiKey && apiKey !== '{{ apiKey }}' && host) mutation.initAppBridge({apiKey, host})
+        },
+        checkUpdate: function () {
+            this.$http.get(getApi('check', 'status')).then(({data}) => this.change = data?.data.change).catch(errorCB)
+        },
+        reinstall: function () {
+            this.$http.get(getApi('check', 'reinstall')).then(({data}) => window.location.href = data?.data).catch(errorCB)
+        }
+    },
     mounted() {
-        // Init App Bridge
-        const apiKey = window?.AppInfo.apiKey,
-            host = window?.AppInfo.host;
-        this.$set(this.$http.defaults.headers.common, 'Authorization', `Bearer ${window?.AppInfo.jwtToken}`)
-        if (apiKey && host) mutation.initAppBridge({apiKey, host})
+        this.initial()
+        this.checkUpdate()
     }
 }
 </script>
--- backend/app/routes/admin.py
+++ backend/app/routes/admin.py
@@ -6,8 +6,10 @@
 @Author: Leo Chen <leo.cxy88@gmail.com>
 @Date: 2020-12-14 11:28
 """
-from flask import Blueprint
+from flask import Blueprint, g, url_for
+from os import environ
 # Custom Modules
+from app.models.shopify import Store
 from app.utils.base import check_jwt, refresh_jwt_token as jsonify
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
@@ -18,6 +20,28 @@ admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 def test():
     """ This is a test admin's api """
     return jsonify()
+
+
+@admin_bp.route('/check/<action>', methods=['GET'], endpoint='check_action')
+@check_jwt
+def check_actions(action):
+    """ Check API Scopes update to date or not """
+    if action not in ['status', 'reinstall']:
+        return jsonify()
+    store = Store.query.filter_by(id=g.store_id).first()
+    if action == 'status':
+        current_scopes = store.scopes.lower().split(',')
+        scopes = environ.get('SCOPES', '').lower().split(',')
+        removes = [v for v in current_scopes if v not in scopes]
+        adds = [v for v in scopes if v not in current_scopes]
+        return jsonify(data=dict(
+            removes=removes,
+            adds=adds,
+            change=len(removes) != 0 or len(adds) != 0
+        ))
+    else:
+        url = url_for('shopify.install', shop=store.key, _external=True, _scheme='https')
+        return jsonify(data=url)
 
 
 __all__ = [admin_bp]
